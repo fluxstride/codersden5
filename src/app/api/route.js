332fc4d958a98ddc328cd99b797d82e3318d4b54
Almuhandith <almuhandith1497@gmail.com>
2023-06-10 13:06:59 +0100

    read and write data

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -3,8 +3,18 @@ import path from 'path'
 import { promises as fs} from 'fs'
  
 export async function GET() {
-  let data = await fs.readFile(path.join(__dirname, "data.json"))
-  console.log(data)
+    let dataDir = path.join(process.cwd(), "json")
+    async function readDb() {
+        let data = await fs.readFile(dataDir+"/data.json")
+        return(JSON.parse(data))
+    }
+  
+  
+  console.log(await readDb())
+
+  fs.writeFile(dataDir+"/db.json", JSON.stringify({message: "database"}))
+
+
  
   return NextResponse.json({message: "Hello world"})
 }
